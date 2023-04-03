622db9f92cbe75656051cfe036fc021b38052324
Artem Obukhov <arteam_101@mail.ru>
2023-04-03 20:44:46 +0700

    Score

---
+++ Assets/Scripts/Collectables/CollectableControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class CollectableControl : MonoBehaviour
+{
+    public static int score = 0;
+    public GameObject scoreDisplay;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (scoreDisplay != null)
+        {
+            scoreDisplay.GetComponent<TextMeshProUGUI>().text = score.ToString();
+        }
+    }
+}
